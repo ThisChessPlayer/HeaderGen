3892d5188e66fc54b274d66f46dee8172df10a43
thischessplayer <jasonma5501@gmail.com>
2017-05-14 17:44:11 -0700

    neatened up source code

--- headerGen.cpp
+++ headerGen.cpp
@@ -38,40 +38,54 @@ int main(int argc, char * argv[]) {
   return 0;
 }
 
+/*----------------------------------------------------------------------------*
+ Method Name: genHeader
+ File:        headerGen.cpp
+  
+ Description: Creates vector of strings to represent the message given
+  
+ Parameter Descriptions:
+ name               description
+ ------------------ -----------------------------------------------
+ message            message to represent using ASCII text
+ <return>           vector of strings representing message
+ *----------------------------------------------------------------------------*/
 vector<string> headerGen::genHeader(const char * message) {
-  //have array of strings for each row
+  //vector of strings representing each row of header
   vector<string> rows;
 
   int i, j;
 
   //init rows
-  for(i = 0; i < this->height; i++) {
+  for(i = 0; i < this->height; i++)
     rows.push_back("");
-  }
-
   
   i = 0;
 
   //for each char in message
   while(message[i] != '\0') {
-    //find alphabet index
+    //find alphabet index with matching letter
     int alphabetInd = message[i] - 'A';
     for(j = 0; j < this->height; j++) {
       //get current last char
       char lastChar = rows[j].back();
 
-      //last char
+      //pop last char
       if(rows[j].length() > 0)
         rows[j].pop_back();
 
+      //add in new letter
       rows[j] = rows[j] + this->alphabet[alphabetInd][j];
 
+      //substitute the old last char in before new letter if it isn't space
       if(lastChar != '\0' && lastChar != ' ')
         rows[j][rows[j].length() - this->width] = lastChar;
     }
     i++;
   }
 
+  //space the message within 80 characters
+  //TODO make this variable
   if(rows[0].length() < 80) {
     int spaces = (80 - rows[0].length()) / 2;
     for(i = 0; i < spaces; i++) {
@@ -81,27 +95,27 @@ vector<string> headerGen::genHeader(const char * message) {
     }
   }
 
+  //add in remaining character if the division comes out even and odd
   if(rows[0].length() < 80) {
     for(i = 0; i < this->height; i++)
       rows[i] = rows[i] + ' ';
   }
 
   return rows;
-
-  //TODO write to file instead of stdout
-  //TODO implement last char in each char thing
-  //TODO set up command line args
-
-  //see how long string is, warn user if > 80 characters, may not be possible with char *
-
-  //keep array of last chars in each row
-  
-  //if either last chars in each row exists or first char in new loaded char is 
-  //not space, then write a char there.
-
-  //if last char, just write out last chars in each row
 }
 
+/*----------------------------------------------------------------------------*
+ Method Name: write
+ File:        headerGen.cpp
+  
+ Description: Opens file and overwrites it with header
+  
+ Parameter Descriptions:
+ name               description
+ ------------------ -----------------------------------------------
+ filepath           location to write header
+ header             vector of strings to write to file
+ *----------------------------------------------------------------------------*/
 void headerGen::write(const char * filepath, vector<string> header) {
   
   ofstream outfile;
@@ -117,24 +131,76 @@ void headerGen::write(const char * filepath, vector<string> header) {
   outfile.close();
 }
 
+/*----------------------------------------------------------------------------*
+ Method Name: headerGen
+ File:        headerGen.cpp
+  
+ Description: Instantiates headerGen object.
+ *----------------------------------------------------------------------------*/
 headerGen::headerGen() {
   this->height = 0;
   this->width = 0;
 }
 
+/*----------------------------------------------------------------------------*
+ Method Name: setSize
+ File:        headerGen.cpp
+  
+ Description: Set size of header, affects way alphabet is read in and header is
+              printed.
+  
+ Parameter Descriptions:
+ name               description
+ ------------------ -----------------------------------------------
+ h                  height of header
+ w                  width of header
+ *----------------------------------------------------------------------------*/
 void headerGen::setSize(int h, int w) {
   this->height = h;
   this->width = w;
 }
 
+/*----------------------------------------------------------------------------*
+ Method Name: getWidth
+ File:        headerGen.cpp
+  
+ Description: Returns width of header
+  
+ Parameter Descriptions:
+ name               description
+ ------------------ -----------------------------------------------
+ <return>           width of header
+ *----------------------------------------------------------------------------*/
 int headerGen::getWidth() {
   return this->width;
 }
 
+/*----------------------------------------------------------------------------*
+ Method Name: getHeight
+ File:        headerGen.cpp
+  
+ Description: Returns height of header
+  
+ Parameter Descriptions:
+ name               description
+ ------------------ -----------------------------------------------
+ <return>           height of header
+ *----------------------------------------------------------------------------*/
 int headerGen::getHeight() {
   return this->height;
 }
 
+/*----------------------------------------------------------------------------*
+ Method Name: loadChars
+ File:        headerGen.cpp
+  
+ Description: Loads alphabet into 2D vector of strings from given filepath.
+  
+ Parameter Descriptions:
+ name               description
+ ------------------ -----------------------------------------------
+ filepath           path to file where alphabet is located
+ *----------------------------------------------------------------------------*/
 void headerGen::loadChars(const char * filepath) {
 
   std::ifstream infile(filepath);
@@ -142,17 +208,17 @@ void headerGen::loadChars(const char * filepath) {
 
   //read in width and height
   infile >> this->width >> this->height;
-  //cout << this->width << " " << this->height << endl;
+
+  //newlines between these and beginning of alphabet
   getline(infile, tempStr);
   getline(infile, tempStr);
 
+  //read in alphabet using width and height.
   for(char c = 'A'; c <= 'Z'; c++) {
     vector<string> v;
-    //cout << "starting " << c << endl;
     for(int i = 0; i < this->height; i++) {
       getline(infile, tempStr);
       v.push_back(tempStr);
-      //cout << tempStr << endl;
     }
 
     this->alphabet.push_back(v);
@@ -160,5 +226,4 @@ void headerGen::loadChars(const char * filepath) {
   }
 
   infile.close();
-  //alphabet.push_back();
 }
